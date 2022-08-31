5dae722ba848be4378da8c0d2247633d52c53ea5
Caio S.G <caiosilvestre.dev@gmail.com>
2022-08-31 10:40:02 -0300

    AddUrlBase

--- src/service/api.tsx
+++ src/service/api.tsx
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const userToken = localStorage.getItem("userToken");
 export const api = axios.create({
-  baseURL: `URL BASE`,
+  baseURL: `https://totraining.herokuapp.com/`,
   headers: { Authorization: `Bearer ${userToken}` },
 });
