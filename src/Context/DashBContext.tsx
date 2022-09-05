96a939cebe6b2a89c261c293a5740ec546e82fe4
Andyara Oss <andyara.oss@gmail.com>
2022-09-05 11:02:43 -0300

    feat: tipagem-userContext

--- src/Context/DashBContext.tsx
+++ src/Context/DashBContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 import { NavigateFunction, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { number } from "yup";
 import { api } from "../service/api";
 
 interface DashBContextProps {
@@ -20,28 +21,29 @@ interface DashBProviderProps {
 interface IUserData {
   acessToken: string;
   user: {
-    email: "uzumaki@mail.com";
-    password: "$2a$10$bZY2KGwiTIpaRXneTdY57.9USquQpaJ6kGoZhk0PIXfL1kUnhB3Z.";
-    name: "Naruto Uzumaki";
-    cellphone: "74 998741477";
-    age: "41";
-    url: "https://static.wikia.nocookie.net/naruto/images/3/33/Naruto_Uzumaki_%28Parte_I_-_HD%29.png/revision/latest?cb=20160316113315&path-prefix=pt-br";
-    confirmPassword: "Senha123@";
-    id: 4;
+    email: string;
+    password: string;
+    name: string;
+    cellphone: number;
+    age: number;
+    url: string;
+    confirmPassword: string;
+    id: number;
   };
 }
+
 interface IExercise {
   name: string;
   rep: string;
   day: string;
-  userId?: string;
+  userId?: number;
 }
 
 interface IExerciseModify {
   name?: string;
   rep?: string;
   day?: string;
-  userId: string;
+  userId: number;
 }
 
 export const DashBContext = createContext<DashBContextProps>(
@@ -51,7 +53,7 @@ export const DashBContext = createContext<DashBContextProps>(
 const DashBProvider = ({ children }: DashBProviderProps) => {
   const [userData, setUserData] = useState<IUserData>();
   const userToken = localStorage.getItem("userToken")!;
-  const id = localStorage.getItem("userId")!;
+  const id = Number(localStorage.getItem("userId"))!;
 
   const [workouts, setWorkouts] = useState<IExercise[]>([]);
 
@@ -89,7 +91,7 @@ const DashBProvider = ({ children }: DashBProviderProps) => {
     });
   };
 
-  const getWorkouts = (id: string) => {
+  const getWorkouts = (id: number) => {
     api
       .get(`/users/${id}?_embed=workouts`)
       .then((resp) => {
--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -8,16 +8,17 @@ import { DashBContext } from "../../Context/DashBContext";
 interface IUserData {
   acessToken: string;
   user: {
-    email: "uzumaki@mail.com";
-    password: "$2a$10$bZY2KGwiTIpaRXneTdY57.9USquQpaJ6kGoZhk0PIXfL1kUnhB3Z.";
-    name: "Naruto Uzumaki";
-    cellphone: "74 998741477";
-    age: "41";
-    url: "https://static.wikia.nocookie.net/naruto/images/3/33/Naruto_Uzumaki_%28Parte_I_-_HD%29.png/revision/latest?cb=20160316113315&path-prefix=pt-br";
-    confirmPassword: "Senha123@";
-    id: 4;
+    email: string;
+    password: string;
+    name: string;
+    cellphone: number;
+    age: number;
+    url: string;
+    confirmPassword: string;
+    id: number;
   };
 }
+
 const Dashboard = () => {
   const { getUserData, userData } = useContext(DashBContext);
   console.log(userData);
