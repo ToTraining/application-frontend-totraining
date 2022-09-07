67e8b31274e0233ed3f877db14d54d48c9199b0a
Caio S.G <caiosilvestre.dev@gmail.com>
2022-09-07 17:40:40 -0300

    Add framer motion

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -5,36 +5,44 @@ import logo from "../../assets/logo.png";
 import { DashBContext } from "../../Context/DashBContext";
 import ModalEdiProfile from "./ModalHeader";
 import { CardDay } from "../../Components/CardDay";
+import { motion } from "framer-motion";
 
 const Dashboard = () => {
   const { userData, domingo, segunda, terca, quarta, quinta, sexta, sabado } =
     useContext(DashBContext);
 
   return (
-    <Body>
-      <header>
-        <img src={logo} alt="ToTraining" />
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.3 }}
+    >
+      <Body>
+        <header>
+          <img src={logo} alt="ToTraining" />
 
-        <ModalEdiProfile />
-      </header>
-      <MainHeader>
-        <div>
-          <h2>{userData.name}</h2>
-          <ModalExercises />
-        </div>
-      </MainHeader>
-      <main>
-        <CardsContainer>
-          <CardDay titulo="Domingo" workoutDay={domingo} />
-          <CardDay titulo="Segunda-feira" workoutDay={segunda} />
-          <CardDay titulo="Terça-feira" workoutDay={terca} />
-          <CardDay titulo="Quarta-feira" workoutDay={quarta} />
-          <CardDay titulo="Quinta-feira" workoutDay={quinta} />
-          <CardDay titulo="Sexta-feira" workoutDay={sexta} />
-          <CardDay titulo="Sábado" workoutDay={sabado} />
-        </CardsContainer>
-      </main>
-    </Body>
+          <ModalEdiProfile />
+        </header>
+        <MainHeader>
+          <div>
+            <h2>{userData.name}</h2>
+            <ModalExercises />
+          </div>
+        </MainHeader>
+        <main>
+          <CardsContainer>
+            <CardDay titulo="Domingo" workoutDay={domingo} />
+            <CardDay titulo="Segunda-feira" workoutDay={segunda} />
+            <CardDay titulo="Terça-feira" workoutDay={terca} />
+            <CardDay titulo="Quarta-feira" workoutDay={quarta} />
+            <CardDay titulo="Quinta-feira" workoutDay={quinta} />
+            <CardDay titulo="Sexta-feira" workoutDay={sexta} />
+            <CardDay titulo="Sábado" workoutDay={sabado} />
+          </CardsContainer>
+        </main>
+      </Body>
+    </motion.div>
   );
 };
 
